211a1dafd2127129d246cb7b59251ca33e067a72
Anthony Luc <anthonyvluc@gmail.com>
2024-03-19 00:50:19 -0400

    Update

--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -90,6 +90,7 @@ local plugins = {
     "tell-k/vim-autopep8",
     ft = "python",
     init = function ()
+      vim.g.autopep8_max_line_length=79
       vim.g.autopep8_disable_show_diff = 1
       vim.g.autopep8_on_save = 1
     end,
