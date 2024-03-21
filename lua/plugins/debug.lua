28f87be0ee39a7a4cf4fbb6daaf8901f27444e3d
Anthony Luc <anthonyvluc@gmail.com>
2024-03-21 01:03:45 -0400

    Update

--- lua/plugins/debug.lua
+++ lua/plugins/debug.lua
@@ -5,53 +5,15 @@ local plugins = {
     {
         "mfussenegger/nvim-dap",
         lazy = false,
+        dependencies = {
+            "suketa/nvim-dap-ruby",
+        },
         config = function()
             local dap = require("dap")
             --
             -- Ruby
             --
-            dap.adapters.ruby = function(callback, config)
-                callback({
-                    type = "server",
-                    host = "127.0.0.1",
-                    port = "${port}",
-                    executable = {
-                        command = "bundle",
-                        args = {
-                            "exec",
-                            "rdbg",
-                            "-n",
-                            "--open",
-                            "--port",
-                            "${port}",
-                            "-c",
-                            "--",
-                            "bundle",
-                            "exec",
-                            config.command,
-                            config.script,
-                        },
-                    },
-                })
-            end
-            dap.configurations.ruby = {
-                {
-                    type = "ruby",
-                    name = "debug current file",
-                    request = "attach",
-                    localfs = true,
-                    command = "ruby",
-                    script = "${file}",
-                },
-                {
-                    type = "ruby",
-                    name = "run current spec file",
-                    request = "attach",
-                    localfs = true,
-                    command = "rspec",
-                    script = "${file}",
-                },
-            }
+            require("dap-ruby").setup()
             --
             -- C++
             --
